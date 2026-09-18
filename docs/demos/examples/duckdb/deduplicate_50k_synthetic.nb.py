3183b351b7be19de63f75fc636b207dbce1c7798
Robin Linacre <robinlinacre@hotmail.com>
2026-09-18 10:31:29 +0100

    :build: update documentation workflow to collect and publish generated notebooks :docs: fix Colab link in deduplicate example notebook

--- docs/demos/examples/duckdb/deduplicate_50k_synthetic.nb.py
+++ docs/demos/examples/duckdb/deduplicate_50k_synthetic.nb.py
@@ -19,7 +19,7 @@
 #
 
 # %% [markdown]
-# <a target="_blank" href="https://colab.research.google.com/github/moj-analytical-services/splink/blob/master/docs/demos/examples/duckdb/deduplicate_50k_synthetic.ipynb">
+# <a target="_blank" href="https://colab.research.google.com/github/RobinL/splink/blob/ipynbs/docs/demos/examples/duckdb/deduplicate_50k_synthetic.ipynb">
 #   <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
 # </a>
 #
